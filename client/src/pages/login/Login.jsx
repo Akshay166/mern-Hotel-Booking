dad059718390fdfd57271ad4b873bb3103423854
Akshay1718 <86643106+Akshay1718@users.noreply.github.com>
2023-03-22 17:37:28 +0530

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -22,9 +22,9 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("https://crew-jhsb.onrender.com/auth/login", credentials);
+      const res = await axios.post("/auth/login", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
-      navigate("https://crew-jhsb.onrender.com/")
+      navigate("/")
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
