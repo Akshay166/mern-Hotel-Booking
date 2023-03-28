18a1526f74186ca8f8bab43b54cec3e7dba13bf9
Akshay166 <68295230+Akshay166@users.noreply.github.com>
2023-03-28 14:09:10 +0530

    Update Reserve.jsx

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -10,7 +10,7 @@ import { useNavigate } from "react-router-dom";
 
 const Reserve = ({ setOpen, hotelId }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
-  const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
+  const { data, loading, error } = useFetch(`https://fty-c00x.onrender.com/api/hotels/room/${hotelId}`);
   const { dates } = useContext(SearchContext);
 
   console.log(data);
